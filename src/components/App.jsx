563e033cfa79803111ae40e4a80b8e1b9d74218c
Christie Finnie <christie.finnie@gmail.com>
2020-02-22 10:32:51 -0800

    Initial files

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to week 15 lab</p>
+    </div>
+  );
+}
+
+export default App;
